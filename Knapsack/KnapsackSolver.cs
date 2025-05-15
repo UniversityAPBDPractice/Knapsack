f969892364cda892d786c5e8c06638f6b098c789
Oleksandr <milkoaleksandr307@gmail.com>
2025-05-15 23:37:56 +0200

    Finish

---
+++ Knapsack/KnapsackSolver.cs
@@ -0,0 +1,62 @@
+﻿using System.Numerics;
+
+namespace Knapsack;
+
+public class KnapsackSolver
+{
+    private int Size { get; set; }
+    private int[] Weights { get; set; }
+    private int[] Values { get; set; }
+    private int Capacity { get; set; }
+    public KnapsackSolver(int[] weights, int[] values, int capacity)
+    {
+        Size = weights.Length;
+        Weights = weights;
+        Values = values;
+        Capacity = capacity;
+    }
+
+    public long Solve()
+    {
+        return Solve(0, Capacity, 0b0);
+    }
+    
+    long SetBit(long number, int index, bool value)
+    {
+        if (value)
+            return number | (1L << index);      // Set bit to 1
+        else
+            return number & ~(1L << index);     // Set bit to 0
+    }
+
+    public int CalculateValue(long knapsack)
+    {
+        int value = 0;
+        for (int i = 0; i < Size; i++)
+        {
+            if (((knapsack >> i) & 1) == 1)
+            {
+                value += Values[i];
+            }
+        }
+
+        return value;
+    }
+
+    public long Solve(int n, int c, long knapsack)
+    {
+        if (n == Size || c == 0) return knapsack;
+        int itemWeight = Weights[n];
+        if (c < itemWeight)
+        {
+            return Solve(n + 1, c, knapsack);
+        }
+        
+        long taken = Solve(n + 1, c - itemWeight, SetBit(knapsack, n, true));
+        long skipped = Solve(n + 1, c, knapsack);
+
+        if (CalculateValue(taken) > CalculateValue(skipped))
+            return taken;
+        return skipped;
+    }
+}
---
+++ Knapsack/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Knapsack;
+public class Program
+{
+    public static void Main()
+    {
+        // int[] weights = {3, 1, 6, 4, 2, 5, 1, 7, 2, 4};
+        // int[] values  = {7, 4, 9, 15, 13, 10, 2, 6, 12, 16};
+
+        int[] weights = {3, 1, 6, 10, 1, 4, 9, 1, 7, 2, 6, 1, 6, 2, 2, 4, 8, 1, 7, 3, 6, 2, 9, 5, 3, 3, 4, 7, 3, 5, 30, 50};
+        int[] values = {7, 4, 9, 18, 9, 15, 4, 2, 6, 13, 18, 12, 12, 16, 19, 19, 10, 16, 14, 3, 14, 4, 15, 7, 5, 10, 10, 13, 19, 9, 8, 5};
+        var solver = new KnapsackSolver(weights, values, 75);
+        
+        var solution = solver.Solve();
+        var maxVal = solver.CalculateValue(solution);
+        Console.WriteLine(Convert.ToString(solution, 2));
+        Console.WriteLine(maxVal);
+    }
+}
